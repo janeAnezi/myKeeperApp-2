b62ff7cf7c28eab54bd45b6408da89ea904bba39
janeAnezi <110386328+janeAnezi@users.noreply.github.com>
2022-10-25 15:01:28 +0100

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DiamondIcon from "@material-ui/icons/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <DiamondIcon /> Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
